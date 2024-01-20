9420df0ae61aeb45b7d349eedbf4ebe5afb95b4a
jorgechavez99 <jorgeluischavez.jlcm@gmail.com>
2024-01-20 11:19:52 +0100

    develop: initial

---
+++ app.js
@@ -0,0 +1,11 @@
+const express=require('express');
+
+const app= express();
+
+const port=process.env.PORT || 5000;
+
+
+
+app.listen(port,()=>{
+    console.log(`Se establecio conexion con el servidor ${port}`)
+})
